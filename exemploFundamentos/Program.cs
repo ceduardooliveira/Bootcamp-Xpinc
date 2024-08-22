c20f459e0204511394a5996df2ef6cb3ed5c2462
ceduardooliveira <c-eduardo94@hotmail.com>
2024-08-22 16:59:02 -0300

    ✨ feat: add variable manipulation

--- exemploFundamentos/Program.cs
+++ exemploFundamentos/Program.cs
@@ -3,18 +3,22 @@
 string apresentacao = "Olá, seja bem vindo!!!";
 
 int quantidade = 1;
-
-double altura = 1.80;
-
-decimal preco = 1.80M;
-
-bool condicao = true;
-
-Console.WriteLine(apresentacao);
 Console.WriteLine("Valor da variável quantidade: " + quantidade);
-Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
-Console.WriteLine("Valor da variável preço: " + preco);
-Console.WriteLine("Valor da variável condição: " + condicao);
+
+quantidade = 10;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+
+
+// double altura = 1.80;
+// decimal preco = 1.80M;
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preço: " + preco);
+// Console.WriteLine("Valor da variável condição: " + condicao);
 
 
 
--- exemploFundamentos/obj/Debug/net8.0/exemploFundamentos.AssemblyInfo.cs
+++ exemploFundamentos/obj/Debug/net8.0/exemploFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6de58dc482925ab88ce2ed81048601d260a456")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb169398569737fdd206b720e8d5278bd14bcdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
