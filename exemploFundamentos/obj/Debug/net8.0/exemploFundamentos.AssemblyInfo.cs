beb169398569737fdd206b720e8d5278bd14bcdc
ceduardooliveira <c-eduardo94@hotmail.com>
2024-08-22 16:42:16 -0300

    ✨ feat: add variable test

--- exemploFundamentos/obj/Debug/net8.0/exemploFundamentos.AssemblyInfo.cs
+++ exemploFundamentos/obj/Debug/net8.0/exemploFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd9c202183d1abd3c1bb7be8de2baa921bad70df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6de58dc482925ab88ce2ed81048601d260a456")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
