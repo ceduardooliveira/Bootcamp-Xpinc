37cd9e7e67271877fd8e6705324b7a51102cac66
ceduardooliveira <c-eduardo94@hotmail.com>
2024-08-22 17:08:37 -0300

    ✨ feat: add datetime type

--- exemploFundamentos/Program.cs
+++ exemploFundamentos/Program.cs
@@ -1,12 +1,19 @@
 ﻿using exemploFundamentos.Models;
 
-string apresentacao = "Olá, seja bem vindo!!!";
 
-int quantidade = 1;
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
 
-quantidade = 10;
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+
+
+// string apresentacao = "Olá, seja bem vindo!!!";
+
+// int quantidade = 1;
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+// quantidade = 10;
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
 
 
 
--- exemploFundamentos/obj/Debug/net8.0/exemploFundamentos.AssemblyInfo.cs
+++ exemploFundamentos/obj/Debug/net8.0/exemploFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb169398569737fdd206b720e8d5278bd14bcdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c20f459e0204511394a5996df2ef6cb3ed5c2462")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
