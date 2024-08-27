1155c41cffbe4cdbecb275fcc9c3b3b4c3daf199
ceduardooliveira <c-eduardo94@hotmail.com>
2024-08-27 13:59:39 -0300

    ✨ feat: add trigonometric function

--- exemploFundamentos/Program.cs
+++ exemploFundamentos/Program.cs
@@ -7,10 +7,12 @@ Calculadora calc = new Calculadora();
 // calc.Multiplicar(15,45);
 // calc.Dividir(2,2);
 
-calc.Potencia(3,3);
+//calc.Potencia(3,3);
 
 
-
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
 
 
 
--- exemploFundamentos/obj/Debug/net8.0/exemploFundamentos.AssemblyInfo.cs
+++ exemploFundamentos/obj/Debug/net8.0/exemploFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3743780455cba0819fc03923989864854e2225cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7024bf8c15130ac56638700b6eef82f3ed93d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
