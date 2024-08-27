2c7024bf8c15130ac56638700b6eef82f3ed93d7
ceduardooliveira <c-eduardo94@hotmail.com>
2024-08-27 13:46:26 -0300

    ✨ feat: add example power

--- exemploFundamentos/obj/Debug/net8.0/exemploFundamentos.AssemblyInfo.cs
+++ exemploFundamentos/obj/Debug/net8.0/exemploFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f5a0e77da70168b5fa0ee6d6f39c0fb0e6cadec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3743780455cba0819fc03923989864854e2225cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
