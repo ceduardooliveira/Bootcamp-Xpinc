71b2656d13c4b93be3e8d2545c0d09a65539602c
ceduardooliveira <c-eduardo94@hotmail.com>
2024-08-28 17:24:20 -0300

    ✨ feat: add create project structure

--- exemploFundamentos.Common/Models/Pessoa.cs
+++ exemploFundamentos.Common/Models/Pessoa.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace exemploFundamentos.Models
+namespace exemploFundamentos.Common.Models
 {
     public class Pessoa
     {
--- exemploFundamentos/Program.cs
+++ exemploFundamentos/Program.cs
@@ -1,45 +1,63 @@
-﻿using System.Reflection;
-using exemploFundamentos.Models;
+﻿using exemploFundamentos.Common.Models;
 
-string opcao;
-bool exibirMenu = true;
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Buta";
+pessoa1.Idade = 20;
+pessoa1.Apresentar();
 
-while(exibirMenu)
-{
-    Console.Clear();
-    Console.WriteLine("Digite uma opçao");
-    Console.WriteLine("1 - Cadastrar cliente.");
-    Console.WriteLine("2 - Buscar cliente.");
-    Console.WriteLine("3 - Apagar cliente.");
-    Console.WriteLine("4 - Encerrar.");
 
-    opcao = Console.ReadLine();
 
-    switch(opcao)
-    {
-        case "1":
-            Console.WriteLine("Cadastro do cliente");
-            break;
 
-        case "2":
-            Console.WriteLine("Busca cliente");
-            break;
 
-        case "3":
-            Console.WriteLine("Apagar cliente");
-            break;   
 
-        case "4":
-            Console.WriteLine("Encerrar");
-            exibirMenu = false;
-            //Environment.Exit(0);
-            break;  
 
-        default:
-            Console.WriteLine("Opção Inválida");   
-            break;   
-    }
-}
+
+
+
+
+
+
+
+
+// string opcao;
+// bool exibirMenu = true;
+
+// while(exibirMenu)
+// {
+//     Console.Clear();
+//     Console.WriteLine("Digite uma opçao");
+//     Console.WriteLine("1 - Cadastrar cliente.");
+//     Console.WriteLine("2 - Buscar cliente.");
+//     Console.WriteLine("3 - Apagar cliente.");
+//     Console.WriteLine("4 - Encerrar.");
+
+//     opcao = Console.ReadLine();
+
+//     switch(opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("Cadastro do cliente");
+//             break;
+
+//         case "2":
+//             Console.WriteLine("Busca cliente");
+//             break;
+
+//         case "3":
+//             Console.WriteLine("Apagar cliente");
+//             break;   
+
+//         case "4":
+//             Console.WriteLine("Encerrar");
+//             exibirMenu = false;
+//             //Environment.Exit(0);
+//             break;  
+
+//         default:
+//             Console.WriteLine("Opção Inválida");   
+//             break;   
+//     }
+// }
 
 
 
