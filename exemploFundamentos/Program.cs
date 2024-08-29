da8042cbd5d53e87edf004d119d9ec1649de0f27
ceduardooliveira <c-eduardo94@hotmail.com>
2024-08-29 14:13:22 -0300

    ✨ feat: add resize array

--- exemploFundamentos/Program.cs
+++ exemploFundamentos/Program.cs
@@ -7,6 +7,8 @@ arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 arrayInteiros[3] = 1;
 
+Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
 
 Console.WriteLine("Percorrendo o Array com o FOR");
 for(int contador = 0; contador < arrayInteiros.Length; contador++)
@@ -15,14 +17,24 @@ for(int contador = 0; contador < arrayInteiros.Length; contador++)
 }
 
 
-Console.WriteLine("Percorrendo o Array com o FOREACH");
 
-int contadorForeach = 0;
-foreach(int valor in arrayInteiros)
-{
-    Console.WriteLine($"Posição Nº {contadorForeach} - {valor}");
-    contadorForeach++;
-}
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("Percorrendo o Array com o FOREACH");
+
+// int contadorForeach = 0;
+// foreach(int valor in arrayInteiros)
+// {
+//     Console.WriteLine($"Posição Nº {contadorForeach} - {valor}");
+//     contadorForeach++;
+// }
 
 
 
